ad2a22c9cf555dc0636f5df9601988efc97aff9f
juanber <juanber@misena.edu.co>
2021-10-29 11:48:22 -0500

    Se cambia instalación de docker con apt-get, dado que la imagen es ubuntu y no centos

--- scripts/instalar-docker.sh
+++ scripts/instalar-docker.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Instalar Docker
-sudo yum install docker-ce docker-ce-cli containerd.io
-sudo systemctl start docker
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io
 
 #Agregar el usuario ubuntu al grupo docker
 sudo groupadd docker
