43a6313013c2b2daeeb90797801d6bf7f65edf9d
juanber <juanber@misena.edu.co>
2021-10-29 21:49:41 -0500

    Se agrega creación de security group, haciendo uso de dynamic para crear las reglas de ingress que deseemos

--- main.tf
+++ main.tf
@@ -7,3 +7,33 @@ resource "aws_instance" "tf_instance_test" {
     instance_type = var.instance_type
     tags = var.tags
 }
+
+resource "aws_security_group" "ssh_connection" {
+    name        = var.sg_name
+    description = "Enable SSH access via port 22"
+    dynamic "ingress" {
+        for_each = var.ingress_rules
+
+        content {
+            from_port   = var.ingress_from_port
+            to_port     = var.ingress_to_port
+            protocol    = var.ingress_protocol
+            cidr_blocks = var.ingress_cidr_blocks
+        }
+
+    }
+
+    egress = [
+        {
+            from_port        = 0
+            to_port          = 0
+            protocol         = "-1"
+            cidr_blocks      = ["0.0.0.0/0"]
+            ipv6_cidr_blocks = ["::/0"]
+        }
+    ]
+
+    tags = {
+        Name = "allow_tls"
+    }
+}
